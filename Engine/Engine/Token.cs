b199c18de91e94640828f0b2c99728916640f83d
Nifemi <deyeminifemi@gmail.com>
2021-08-18 21:00:41 +0100

    Token files

---
+++ Engine/Engine/Token.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Engine {
+    public class Token {
+        private string id;
+        private List<TokenItem> documents;
+        private int currentDocumentId = -1;
+
+        public void AddItem(int documentId, int position) {
+            if (currentDocumentId == documentId) {
+                TokenItem currentDocument = documents[documents.Count - 1];
+                currentDocument.AddPosition(position);
+            } else {
+                currentDocumentId = documentId;
+                TokenItem newDocument = new TokenItem(position);
+                documents.Add(newDocument);
+            }
+        }
+    }
+}
---
+++ Engine/Engine/TokenItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Engine {
+    public class TokenItem {
+        private int delta;
+        private List<int> positions;
+        private int frequency;
+
+        public TokenItem(int position) {
+            positions.Add(position);
+        }
+        public void AddPosition(int position) {
+            int lastItem = positions[positions.Count - 1];
+            //Delta system
+            positions.Add(position - lastItem);
+        }
+    }
+}
