f41b672bb156b7aa46a35f5439b1ff8a1db09cc3
Nifemi <deyeminifemi@gmail.com>
2021-08-21 18:27:20 +0100

    Index class with words

---
+++ Engine/Engine/Index.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Engine {
+    public class Index {
+        public Dictionary<string, Token> tokens = new Dictionary<string, Token>();
+
+        public void AddWord(string word, int documentId, int position) {
+            Token wordItem;
+            if (tokens.ContainsKey(word)) {
+                wordItem = tokens[word];
+            }
+            else {
+                wordItem = new Token(word);
+                tokens[word] = wordItem;
+            }
+            
+            wordItem.AddItem(documentId, position);
+        }
+    }
+}
