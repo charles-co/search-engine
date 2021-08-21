f8a0cfbff18ef9f0cf1765075f07da6009c16642
Nifemi <deyeminifemi@gmail.com>
2021-08-21 18:25:40 +0100

    Fixed crash

--- Engine/Engine/Token.cs
+++ Engine/Engine/Token.cs
@@ -3,9 +3,12 @@
 namespace Engine {
     public class Token {
         private string id;
-        private List<TokenItem> documents;
+        private List<TokenItem> documents = new List<TokenItem>();
         private int currentDocumentId = -1;
 
+        public Token(string id) {
+            this.id = id;
+        }
         public void AddItem(int documentId, int position) {
             if (currentDocumentId == documentId) {
                 TokenItem currentDocument = documents[documents.Count - 1];
--- Engine/Engine/TokenItem.cs
+++ Engine/Engine/TokenItem.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 namespace Engine {
     public class TokenItem {
         private int delta;
-        private List<int> positions;
+        private List<int> positions = new List<int>();
         private int frequency;
 
         public TokenItem(int position) {
             positions.Add(position);
         }
+        
         public void AddPosition(int position) {
             int lastItem = positions[positions.Count - 1];
             //Delta system
