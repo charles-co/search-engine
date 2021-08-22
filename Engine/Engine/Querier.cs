5f536e9087d6628d98074f90f3d88710dc25d976
Nifemi <deyeminifemi@gmail.com>
2021-08-22 21:45:13 +0100

    Base querying Adding to queue etc

---
+++ Engine/Engine/Querier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Priority_Queue;
+
+namespace Engine {
+    public class Querier {
+        private FastPriorityQueue<TokenPointer> _queue;
+        
+        public void Search(string query) {
+            var cleanedWords = Utils.CleanAndExtractWords(query);
+            Index searchIndex = new SearchIndex(cleanedWords);
+            GeneratePointers(searchIndex, cleanedWords);
+        }
+
+        private void GeneratePointers(Index targetIndex, string [] words) {
+            _queue = new FastPriorityQueue<TokenPointer>(words.Length);
+            
+            foreach (var word in words) {
+                var pointer = new TokenPointer(targetIndex.tokens[word]);
+                _queue.Enqueue(pointer, pointer.target.documentPosition);
+            }
+        }
+    }
+}
