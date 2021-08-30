f74386af3632781852956504d0e0d99475f33723
Nifemi <deyeminifemi@gmail.com>
2021-08-30 21:29:11 +0100

    Documentation for db document

--- Engine/Engine/DbDocument.cs
+++ Engine/Engine/DbDocument.cs
@@ -6,20 +6,39 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Engine {
+    /// <summary>
+    /// This extends the base document class to add extra field poition to describe the position of the document in the list of documents
+    /// Also contains helper functions for indexing documents and saving documents to the database 
+    /// </summary>
     public class DbDocument : BaseDocument {
         public readonly long Position;
         private static readonly Queue<BaseDocument> ToBeIndexed = new Queue<BaseDocument>();
 
         private static readonly Semaphore SaveDocSemaphore = new Semaphore(1, 1);
+        /// <summary>
+        /// Creates a new instance of <see cref="DbDocument"/>
+        /// </summary>
+        /// <param name="position">Position of document in list of documents</param>
+        /// <param name="documentId">Document uuid</param>
+        /// <param name="url">Url of document</param>
+        /// <param name="name">Name of document</param>
         private DbDocument(long position, string documentId, string url, string name) : base(name, url, documentId) {
             Position = position;
         }
 
+        /// <summary>
+        /// This function adds the document to the indexing queue and saves the document to the database
+        /// </summary>
+        /// <param name="url">Url of document</param>
+        /// <param name="name">Name of document</param>
         public static void IndexDocument(string name, string url) {
             ToBeIndexed.Enqueue(new BaseDocument(name, url));
             SaveToDb();
         }
 
+        /// <summary>
+        /// This saves the document to the database
+        /// </summary>
         private static async void SaveToDb() {
             SaveDocSemaphore.WaitOne();
             var currentDocument = ToBeIndexed.Dequeue();
@@ -44,6 +63,7 @@ namespace Engine {
 
             SaveDocSemaphore.Release();
 
+            //Calls the indexer function to add the document to the queue of items to be indexed
             Indexer.TryIndex(new DbDocument(documentIndex, createdDoc["_id"].ToString(), url, createdDoc["name"].ToString()));
         }
     }
