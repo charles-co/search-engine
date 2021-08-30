f2162223d7caa41d724782fe6d4cce23d9965747
Nifemi <deyeminifemi@gmail.com>
2021-08-30 21:32:56 +0100

    Indexer class documentation

--- Engine/Engine/Indexer.cs
+++ Engine/Engine/Indexer.cs
@@ -6,11 +6,19 @@ using Exception = System.Exception;
 
 
 namespace Engine {
+    /// <summary>
+    /// Indexer class used to index documents
+    /// </summary>
     public static class Indexer {
         private static readonly Queue<DbDocument> ToBeIndexed = new Queue<DbDocument>();
 
         private static readonly Semaphore IndexDocSemaphore = new Semaphore(1, 1);
 
+        /// <summary>
+        /// This is used to extract text from a document url
+        /// </summary>
+        /// <param name="url">This is the url of the target document</param>
+        /// <returns>The extracted text</returns>
         private static string ExtractText(string url) {
             var textExtractor = new TextExtractor();
             var result = Uri.TryCreate(url, UriKind.Absolute, out var uri)
@@ -19,11 +27,19 @@ namespace Engine {
             return contents.Text;
         }
 
+        /// <summary>
+        /// Adds the document to the documents to be indexed and tries to acquire a semaphore for indexing
+        /// </summary>
+        /// <param name="dbDocument">An instance of <see cref="DbDocument"/> which is to be indexed</param>
         public static void TryIndex(DbDocument dbDocument) {
             ToBeIndexed.Enqueue(dbDocument);
             IndexDocument();
         }
 
+        /// <summary>
+        /// Tries to acquire semaphore for indexing
+        /// Calls needed functions for cleaning and indexing the document
+        /// </summary>
         private static async void IndexDocument() {
             IndexDocSemaphore.WaitOne();
             try {
