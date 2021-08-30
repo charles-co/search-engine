d60ce077a88c7795d18fbafb215e108d51c70ef3
Nifemi <deyeminifemi@gmail.com>
2021-08-30 21:12:57 +0100

    Documentation for token

--- Engine/Engine/Token.cs
+++ Engine/Engine/Token.cs
@@ -5,16 +5,29 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 namespace Engine {
+    /// <summary>
+    /// This is a representation of a token in the index. It stores the word, the documents under a token and the number of times that token exists.
+    /// </summary>
     public class Token {
         private readonly string _word;
         public readonly List<TokenItem> Documents = new List<TokenItem>();
         private string _currentDocumentId;
         public int Frequency;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Token"/> class 
+        /// </summary>
+        /// <param name="word">Sets the word that a token is pointing to</param>
         public Token(string word) {
             _word = word;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Token"/> class 
+        /// </summary>
+        /// <param name="word">Sets the word that a token is pointing to</param>
+        /// <param name="initialDocuments">These are the initial documents a token contains. It is useful when loading a token from the database for querying</param>
+        /// <param name="frequency">This is the count of the number of times a token appears across the documents. Useful for querying</param>
         public Token(string word, BsonValue[] initialDocuments, int frequency) {
             _word = word;
             Frequency = frequency;
@@ -27,6 +40,12 @@ namespace Engine {
             }
         }
 
+        /// <summary>
+        /// This adds a new item to the the list of tokens.
+        /// It checks if the document has already been included in our list of documents if it has it adds the item else it deletes it
+        /// </summary>
+        /// <param name="dbDocument">An instance of the <see cref="DbDocument"/> class</param>
+        /// <param name="position">The position of the word within the document</param>
         public void AddItem(DbDocument dbDocument, int position) {
             if (_currentDocumentId == dbDocument.DocumentId) {
                 var currentDocument = Documents[Documents.Count - 1];
@@ -40,6 +59,10 @@ namespace Engine {
             }
         }
 
+        /// <summary>
+        /// This is used to convert the db document instances to a BSON document which is useful for saving to the db
+        /// </summary>
+        /// <returns>Returns an array of BsonArray</returns>
         private BsonArray GetBsonDocuments() {
             var documentsArray = new BsonArray();
             foreach (var document in Documents) {
@@ -55,12 +78,18 @@ namespace Engine {
             return documentsArray;
         }
         
+        /// <summary>
+        /// This saves the current token to the database to the database
+        /// </summary>
         public async Task SaveSelfToDb() {
             var getFilter = Builders<BsonDocument>.Filter.Eq("word", _word);
             var tokensCollection = Connector.GetTokensCollection();
+            
+            //Fetch previous instance of token
             var prevToken = tokensCollection.Find(getFilter).FirstOrDefault();
-
+            
             if (prevToken != null) {
+                //If it already exists update relevant fields
                 var taskBson = prevToken.ToBsonDocument();
                 var previousDocuments = BsonSerializer.Deserialize<BsonArray>(taskBson["documents"].ToJson());
                 var newDocuments = previousDocuments.AddRange(GetBsonDocuments());
@@ -70,6 +99,7 @@ namespace Engine {
                 await tokensCollection.UpdateOneAsync(getFilter, update);
             }
             else {
+                //Create a new token
                 var token = new BsonDocument {
                     {"word", _word},
                     {"documents", GetBsonDocuments()},
